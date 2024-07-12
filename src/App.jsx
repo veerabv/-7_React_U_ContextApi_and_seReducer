7aa31cf53d15e9dfb724e8725ebb67651239e49c
veerabv <bveera0608@gmail.com>
2024-07-12 19:27:33 +0530

    create and providing the context

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Product from './components/Product.jsx'
 import Header from './components/Header.jsx';
 import Shop from './components/Shop.jsx';
 import { DUMMY_PRODUCTS } from './dummy-products.js';
+import { CartContext } from './store/shopping_cart.jsx';
 
 
 function App() {
@@ -67,7 +68,7 @@ function App() {
   }
 
   return (
-    <>
+    <CartContext.Provider>
       <Header
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
@@ -79,7 +80,7 @@ function App() {
           </li>
         ))}
       </Shop>
-    </>
+    </CartContext.Provider>
   );
 }
 
