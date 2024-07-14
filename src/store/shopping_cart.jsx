2f9d914723abeff4218723282fc9508513766075
veerabv <bveera0608@gmail.com>
2024-07-14 13:37:54 +0530

    cleaning the code

--- src/store/shopping_cart.jsx
+++ src/store/shopping_cart.jsx
@@ -67,43 +67,9 @@ export default function CartContextProvider({ children }) {
   const [cartItems, cartItemsDispatch] = useReducer(shoppingCartReducer, {
     items: [],
   });
-  // console.log(cartItems, ":::::::::::::::");
-  // const [shoppingCart, setShoppingCart] = useState({
-  //   items: [],
-  // });
 
   function handleAddItemToCart(id) {
-    // console.log("triggered");
     cartItemsDispatch({ type: "ADD_ITEM", payload: id });
-
-    // setShoppingCart((prevShoppingCart) => {
-    //   const updatedItems = [...prevShoppingCart.items];
-
-    //   const existingCartItemIndex = updatedItems.findIndex(
-    //     (cartItem) => cartItem.id === id
-    //   );
-    //   const existingCartItem = updatedItems[existingCartItemIndex];
-
-    //   if (existingCartItem) {
-    //     const updatedItem = {
-    //       ...existingCartItem,
-    //       quantity: existingCartItem.quantity + 1,
-    //     };
-    //     updatedItems[existingCartItemIndex] = updatedItem;
-    //   } else {
-    //     const product = DUMMY_PRODUCTS.find((product) => product.id === id);
-    //     updatedItems.push({
-    //       id: id,
-    //       name: product.title,
-    //       price: product.price,
-    //       quantity: 1,
-    //     });
-    //   }
-
-    //   return {
-    //     items: updatedItems,
-    //   };
-    // });
   }
 
   function handleUpdateCartItemQuantity(productId, amount) {
@@ -111,28 +77,6 @@ export default function CartContextProvider({ children }) {
       type: "UPDATE_ITEM",
       payload: { id: productId, amount: amount },
     });
-    // setShoppingCart((prevShoppingCart) => {
-    // const updatedItems = [...prevShoppingCart.items];
-    // const updatedItemIndex = updatedItems.findIndex(
-    //   (item) => item.id === productId
-    // );
-
-    // const updatedItem = {
-    //   ...updatedItems[updatedItemIndex],
-    // };
-
-    // updatedItem.quantity += amount;
-
-    // if (updatedItem.quantity <= 0) {
-    //   updatedItems.splice(updatedItemIndex, 1);
-    // } else {
-    //   updatedItems[updatedItemIndex] = updatedItem;
-    // }
-
-    // return {
-    //   items: updatedItems,
-    // };
-    // });
   }
 
   let cntxtValue = {
